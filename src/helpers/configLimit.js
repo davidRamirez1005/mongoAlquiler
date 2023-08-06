2e76d46bf25d123b43af243ce187ad3e957ce15c
davidRamirez1005 <ramirezjd1005@gmail.com>
2023-08-05 22:22:39 -0500

    feat: :sparkles: se aumenta el numero de caracteres
    para no presentar problemas con la data

--- src/helpers/configLimit.js
+++ src/helpers/configLimit.js
@@ -11,7 +11,7 @@ export let limitget = () => {
         // standardHeaders : true,
         // legacyHeaders : false,
         skip: (req, res)=>{
-            if(req.headers["content-length"]>91){
+            if(req.headers["content-length"]>500){
                 res.status(413).send({
                     status:413, 
                     message :"la cantidad de caracteres supera el limite."})
