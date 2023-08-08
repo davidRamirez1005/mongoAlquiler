16931eda4d3bb6bd4f0aea023c3931edaa7e65c1
davidRamirez1005 <ramirezjd1005@gmail.com>
2023-08-08 17:10:17 -0500

    feat: :sparkles: se elimina parte de codigo para usarlo en el middleware del mismo

--- src/routers/usuarioEjemplo.js
+++ src/routers/usuarioEjemplo.js
@@ -2,12 +2,13 @@ import {ObjectId} from 'mongodb'
 import {Router} from 'express';
 import {limitget} from '../helpers/configLimit.js'
 import {con} from '../../db/atlas.js'
+import middlewareUsuario from '../middleware/usuario.js';
 
 
 const appEjemplo = Router();
 
 
-appEjemplo.get('/:id', limitget(), async(req, res) =>{
+appEjemplo.get('/:id', limitget(), middlewareUsuario, async(req, res) =>{
     if(!req.rateLimit) return;
 
     let { id } = req.params
@@ -17,20 +18,9 @@ appEjemplo.get('/:id', limitget(), async(req, res) =>{
     res.send(result)
 })
 
-appEjemplo.post('/', limitget(), async(req, res) => {
-    if(!req.rateLimit) return;
-
-    let db = await con();
-    let usuario = db.collection('usuario');
-    try {
-        let result = await usuario.insertOne(req.body);
-        console.log(result);
-        res.send(':)')
-    } catch (error) {
-        console.log(error.errInfo.details.schemaRulesNotSatisfied[0]);
-        res.send(':(')
-    };
+appEjemplo.post('/', limitget(), middlewareUsuario, async(req, res) => {
     
+    res.send(':)')
 })
 
 export default appEjemplo;
