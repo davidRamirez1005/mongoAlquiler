445fe37ce9292a6a61a8656e5cea9fb57bbd7db2
davidRamirez1005 <ramirezjd1005@gmail.com>
2023-08-09 14:00:42 -0500

    feat: :sparkles: DTO
    para coleccion de usuario y cliente

---
+++ src/routers/storage/clientes.ts
@@ -0,0 +1,44 @@
+import { Expose, Transform } from 'class-transformer';
+import { IsDefined} from 'class-validator';
+export class Client {
+    @Expose({ name: 'ID' })
+    @IsDefined({message: ()=>{ throw {status: 422, message: `La ID es obligatoria`}}})
+    ID: number;
+
+    @Expose({ name: 'Nombre' })
+    @IsDefined({message: ()=>{ throw {status: 422, message: `El Nombre es obligatoria`}}})
+    Nombre: string;
+
+    @Expose({ name: 'Apellido' })
+    @IsDefined({message: ()=>{ throw {status: 422, message: `El Apellido es obligatoria`}}})
+    // @Transform(({ value }) => { if(value) return value ; else "Faker"})
+    Apellido: string;
+
+    @Expose({ name: 'DNI' })
+    @IsDefined({message: ()=>{ throw {status: 422, message: `La DNI es obligatoria`}}})
+    DNI: string;
+
+    @Expose({ name: 'Direccion' })
+    @IsDefined({message: ()=>{ throw {status: 422, message: `La Direccion es obligatoria`}}})
+    Direccion: string;
+
+    @Expose({ name: 'Telefono' })
+    @IsDefined({message: ()=>{ throw {status: 422, message: `La Telefono es obligatoria`}}})
+    Telefono: string;
+
+    @Expose({ name: 'Email' })
+    @IsDefined({message: ()=>{ throw {status: 422, message: `La Email es obligatoria`}}})
+    Email: string;
+
+    constructor(data:Partial<Client>) {
+      Object.assign(this, data);
+      this.ID = 0;
+      this.Nombre = "Faker";
+      this.Apellido = "faker";
+      this.Apellido = "faker";
+      this.DNI = "faker";
+      this.Direccion = "faker";
+      this.Telefono = "faker";
+      this.Email = "faker";
+    }
+}
